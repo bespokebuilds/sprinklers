ebe0da1d80545f17cd43ae40866ac070bea495a3
cv <cv-us@pm.me>
2024-07-02 20:21:00 -0700

    first commit

---
+++ src/lib/navigation.js
@@ -0,0 +1,56 @@
+export const navigation = [
+  {
+    title: 'Introduction',
+    links: [
+      { title: 'Getting started', href: '/' },
+      { title: 'Contributing', href: '/docs/installation' },
+    ],
+  },
+  {
+    title: 'Standard Operating Procedures',
+    links: [
+      { title: 'Project Turnovers', href: '/docs/understanding-caching' },
+      {
+        title: 'Estimations',
+        href: '/docs/predicting-user-behavior',
+      },
+      { title: 'Basics of time-travel', href: '/docs/basics-of-time-travel' },
+      {
+        title: 'Introduction to string theory',
+        href: '/docs/introduction-to-string-theory',
+      },
+      { title: 'The butterfly effect', href: '/docs/the-butterfly-effect' },
+    ],
+  },
+  {
+    title: 'Design',
+    links: [
+      { title: 'Systems', href: '/docs/writing-plugins' },
+      { title: 'Calculations', href: '/docs/neuralink-integration' },
+      { title: 'Product Data', href: '/docs/temporal-paradoxes' },
+      { title: 'Requests For Information', href: '/docs/testing' },
+      { title: 'Compile-time caching', href: '/docs/compile-time-caching' },
+      {
+        title: 'Predictive data generation',
+        href: '/docs/predictive-data-generation',
+      },
+    ],
+  },
+  {
+    title: 'Stages of Design',
+    links: [
+      { title: 'predict', href: '/docs/predict' },
+      { title: 'flush', href: '/docs/flush' },
+      { title: 'revert', href: '/docs/revert' },
+      { title: 'regret', href: '/docs/regret' },
+    ],
+  },
+  {
+    title: 'Contributing',
+    links: [
+      { title: 'How to contribute', href: '/docs/how-to-contribute' },
+      { title: 'Architecture guide', href: '/docs/architecture-guide' },
+      { title: 'Design principles', href: '/docs/design-principles' },
+    ],
+  },
+]
